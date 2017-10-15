7ee7cc53754f73b8f6c30b7f3eb8d85142f9dc20
Afnan Sharif <32818988+Afnan-Sharif@users.noreply.github.com>
2017-10-15 22:49:13 +0600

    Add files via upload

---
+++ decremental.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int i=2;
+    i--;
+    printf("%d\n",i);
+    --i;
+    printf("%d\n",i);
+}
---
+++ incrementAndDecrement.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i=10;
+    i-=2;
+
+    printf("%d",4*i);
+
+
+}
